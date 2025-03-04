11f5d5072621da8d6134e40dd0f35e1fc37ee325
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-03-04 17:43:36 +0100

    start implementing form-article.js with form error validation

--- src/form-article/form-article.js
+++ src/form-article/form-article.js
@@ -3,7 +3,37 @@ import "./form-article-style.scss";
 import "../assets/javascripts/topbar.js";
 
 const form = document.querySelector("form");
+const errorUl = document.querySelector("#errors");
+let errors = [];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+
+  if (isFormValid(article)) {
+    const json = JSON.stringify(article);
+  } else {
+  }
 });
+
+const isFormValid = (article) => {
+  let isValid = true;
+  errors = [];
+  if (!article.author || !article.category || !article.content) {
+    errors.push("Vous devez renseigner tous les champs");
+    isValid = false;
+  }
+
+  if (!isValid) {
+    let errorHTML = "";
+    errors.map((e) => {
+      errorHTML += `<li>${e}</li>`;
+    });
+    errorUl.innerHTML = errorHTML;
+  } else {
+    errorUl.innerHTML = "";
+  }
+
+  return isValid;
+};
